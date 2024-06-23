298badf8d3f57780a13a6a2fc038b624ef5c90f2
navamayoo <navamayoo@gmail.com>
2024-06-23 12:43:45 +0530

    Update index page and install swaggerJs doc

--- index.js
+++ index.js
@@ -2,61 +2,41 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-
+const userRoute = require("./Routers/userRoute");
 
 const MONGO_DB = process.env.MONGO_DB;
 const PORT = process.env.PORT;
 
 
-app.use(express.json());
 
-app.get("/", function (req, res) {
-    res.send("Hello C# Corner.");
-   });
+   app.use(express.json());
+   // Enable CORS for all routes
+   app.use(cors({ origin: 'http://localhost:3000' }));
+   //User Route
+   app.use("/api", userRoute);
    
-
-//app.use(cors({ origin: "http://localhost:3000" }));
-
-// app.post("/api/google-login", async (req, res) => {
-//   const { token } = req.body;
-
-//   try {
-//     // Verify the token using Google API
-//     const ticket = await client.verifyIdToken({
-//       idToken: token,
-//       audience: CLIENT_ID,
-//     });
-
-//     const payload = ticket.getPayload();
-//     const { sub, email, name, picture } = payload;
-
-//     // Check if the user exists in the MongoDB database
-//     let user = await User.findOne({ googleId: sub });
-//     if (!user) {
-//       // Create a new user record if not exists
-//       user = new User({
-//         googleId: sub,
-//         email,
-//         name,
-//         picture,
-//       });
-//       await user.save();
-//     }
-
-//     // Generate a session token for the authenticated user
-//     const sessionToken = jwt.sign(
-//       { userName: user.name, email: user.email, picture: user.picture },
-//       JWT_SECRET,
-//       { expiresIn: "1h" }
-//     );
-
-//     // Send the session token to the client
-//     res.json({ success: true, sessionToken });
-//   } catch (error) {
-//     console.error("Error verifying token:", error);
-//     res.status(401).json({ success: false, message: "Invalid token" });
-//   }
-// });
+   const options = {
+     definition: {
+       openapi: '3.0.0',
+       info: {
+         title: 'Booking Management System',
+         version: '1.0.0',
+         description: 'Booking Management System covered Create, Read, Update, and Delete operations using a Node.js API',
+       },  
+       servers:[
+         {url:'http://localhost:5000/api'},
+       ],
+     },
+   
+     apis: ['./routes/*.js'], 
+   };
+   
+   const specs = swaggerJsdoc(options);
+   
+   app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
+   
+   
+   
 
 mongoose
   .connect(MONGO_DB)
